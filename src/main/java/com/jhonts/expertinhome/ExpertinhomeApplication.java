e30355293dbaedc09b94a1a8a20c92724e263a88
jhonts <jjcinformatik@gmail.com>
2019-07-25 21:49:53 +0100

    initial commit for expert in home

---
+++ src/main/java/com/jhonts/expertinhome/ExpertinhomeApplication.java
@@ -0,0 +1,13 @@
+package com.jhonts.expertinhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertinhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertinhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhonts/expertinhome/ExpertinhomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jhonts.expertinhome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpertinhomeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
